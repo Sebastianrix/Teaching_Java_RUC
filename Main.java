16476e3953a2d8891b948360d141d50da9d83d65
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2025-10-12 23:11:55 +0200

    Update

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+        
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        int[] inputArray = new int[24];
+        
+        for (int x = 0; x < inputArray.length; x++) {
+            inputArray[x] = scanner.nextInt();
+        }
+        
+        
+        for (int y = 0; y < inputArray.length; y++) {
+              System.out.println(y+". List is as this :"+inputArray[y]);
+            }
+        }
     }
-}
+
